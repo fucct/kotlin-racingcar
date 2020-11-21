9376996da77925e91b4eb50d27df8e53e949d688
dd <dqrd123@gmail.com>
2020-11-21 21:33:58 +0900

    feat: CarName 테스트 및 기본 구현

--- src/main/kotlin/domain/CarName.kt
+++ src/main/kotlin/domain/CarName.kt
@@ -1,3 +1,9 @@
 package domain
 
-data class CarName(val name: String)
+data class CarName(val name: String?){
+    init{
+        if (name == null || name.length !in 2..5) {
+            throw IllegalArgumentException("자동차 이름은 2~5자를 입력하셔야 합니다.")
+        }
+    }
+}
---
+++ src/test/kotlin/domain/CarNameTest.kt
@@ -0,0 +1,34 @@
+package domain
+
+import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.core.spec.style.BehaviorSpec
+import io.kotest.matchers.should
+import io.kotest.matchers.shouldBe
+import io.kotest.matchers.string.startWith
+import io.kotest.matchers.types.shouldBeTypeOf
+
+internal class CarNameTest : BehaviorSpec({
+    given("CarName 생성 시"){
+        `when`("2~5글자의 CarName을 입력하면"){
+            then("정상적으로 CarName을 생성한다"){
+                val carName = CarName("pobi")
+                carName.shouldBeTypeOf<CarName>()
+                carName.name shouldBe "pobi"
+            }
+        }
+
+        `when`("2~5글자가 아닌 CarName을 입력하면"){
+            then("예외를 반환한다"){
+                val exception = shouldThrow<IllegalArgumentException> { CarName("d") }
+                exception.message should startWith("자동차 이름은 2~5자를 입력하셔야 합니다.")
+            }
+        }
+
+        `when`("공백(NULL) CarName을 입력하면"){
+            then("예외를 반환한다"){
+                val exception = shouldThrow<IllegalArgumentException> { CarName(null) }
+                exception.message should startWith("자동차 이름은 2~5자를 입력하셔야 합니다.")
+            }
+        }
+    }
+})
