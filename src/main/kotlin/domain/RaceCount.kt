acbced0aa60e36dd9f0ffbd42e8275e2914ffc58
dd <dqrd123@gmail.com>
2020-11-22 22:34:33 +0900

    refactor: RaceCount range 상수화

--- src/main/kotlin/domain/RaceCount.kt
+++ src/main/kotlin/domain/RaceCount.kt
@@ -1,8 +1,13 @@
 package domain
 
 data class RaceCount(val count: Long) {
+    companion object {
+        const val MAX_COUNT: Int = 200
+        const val MIN_COUNT: Int = 0
+    }
+
     init {
-        if (count <= 0) {
+        if (count <= MIN_COUNT || count > MAX_COUNT) {
             throw IllegalArgumentException("Race Count는 0 이하일 수 없습니다.")
         }
     }
