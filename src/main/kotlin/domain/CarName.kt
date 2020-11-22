726ebecdcce0c0913e85dc933411c158c8369158
dd <dqrd123@gmail.com>
2020-11-22 22:24:19 +0900

    refactor: CarName trim 추가

--- src/main/kotlin/domain/CarName.kt
+++ src/main/kotlin/domain/CarName.kt
@@ -2,7 +2,8 @@ package domain
 
 data class CarName(val name: String?){
     init{
-        if (name == null || name.length !in 2..5) {
+        val carName = name?.trim()
+        if (carName == null || carName.length !in 2..5) {
             throw IllegalArgumentException("자동차 이름은 2~5자를 입력하셔야 합니다.")
         }
     }
