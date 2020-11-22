b41df514ae93b4cd51da75cb913f6c80ebc91ce8
dd <dqrd123@gmail.com>
2020-11-22 22:34:07 +0900

    refactor: Engine Default Generator 삭제

--- src/main/kotlin/domain/Engine.kt
+++ src/main/kotlin/domain/Engine.kt
@@ -5,7 +5,7 @@ class Engine() {
         const val MOVE_CRITERIA = 4
     }
 
-    fun accelerate(randomGenerator: RandomGenerator = DefaultRandomGenerator()): Int {
+    fun accelerate(randomGenerator: RandomGenerator): Int {
         val randomNumber = randomGenerator.generate()
         if (randomNumber >= MOVE_CRITERIA) {
             return 1
